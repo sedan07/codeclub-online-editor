b38b101b6dcb85fabf3930d372ca75cb059acddc
unknown <Seb.Dangerfield@TG010.TG.local>
2013-11-21 20:32:35 +0000

    Got content to go into the iframe

--- public_html/js/controllers/EditorController.js
+++ public_html/js/controllers/EditorController.js
@@ -1,5 +1,11 @@
 angular.module('ccApp.controllers', []).
-  controller('EditorController', ['$scope', function($scope) {
+  controller('EditorController', ['$scope', '$document', function($scope, $document) {
     
     $scope.test = 'boo!';
+    
+    $scope.publish = function() {
+        var frame = angular.element(document.querySelector('#previewWindow'))[0];
+        var doc = frame.contentDocument || frame.contentWindow.document;
+        angular.element(doc).find('body').html(editor.getValue());
+    };
   }]);
