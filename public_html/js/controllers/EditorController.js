809a6d68f23a42afea5a2d479fa5183cca3adf62
unknown <Seb.Dangerfield@TG010.TG.local>
2013-11-21 21:35:10 +0000

    Added CSS to iframe + tab switchers + local storage for saving your files

--- public_html/js/controllers/EditorController.js
+++ public_html/js/controllers/EditorController.js
@@ -1,11 +1,47 @@
-angular.module('ccApp.controllers', []).
-  controller('EditorController', ['$scope', '$document', function($scope, $document) {
+angular.module('ccApp.controllers', ['LocalStorageModule']).
+  controller('EditorController', ['$scope', 'localStorageService', function($scope, localStorageService) {
     
-    $scope.test = 'boo!';
+    $scope.files = {
+        html: '',
+        css: ''
+    };
+    
+    $scope.currentTab = 'html';
     
     $scope.publish = function() {
         var frame = angular.element(document.querySelector('#previewWindow'))[0];
         var doc = frame.contentDocument || frame.contentWindow.document;
-        angular.element(doc).find('body').html(editor.getValue());
+        angular.element(doc).find('body').html($scope.files.html);
+        
+        var styles = angular.element('<style type="text/css"></style>')
+        styles.html($scope.files.css);
+        angular.element(doc).find('head').html('');
+        angular.element(doc).find('head').append(styles);
     };
+    
+    $scope.changeTab = function(newTab) {
+        $scope.currentTab = newTab;
+        $scope.editor.setValue($scope.files[newTab]);
+        $scope.editor.getSession().setMode("ace/mode/"+newTab);
+    }
+    
+    main = function() {
+        var cache = localStorageService.get('data_cache');
+        if (cache) {
+            $scope.files = JSON.parse(cache);
+        }
+        $scope.editor = ace.edit("editor");
+        $scope.editor.setTheme("ace/theme/monokai");
+        $scope.changeTab('html');
+        
+        $scope.editor.getSession().on('change', function(e) {
+            $scope.files[$scope.currentTab] = $scope.editor.getValue();
+            localStorageService.add('data_cache',JSON.stringify($scope.files));
+        });
+    };
+    
+    
+    
+    main();
+    
   }]);
