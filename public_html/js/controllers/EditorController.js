6bf848eb7667fa836be4304cc68eb95b447d17a0
Seb Dangerfield <seb.dangerfield@pocketgizmo.com>
2013-11-25 20:56:23 +0000

    editor tabs now work as before. Sessions allow each tab to hold it's own state info and undo history

--- public_html/js/controllers/EditorController.js
+++ public_html/js/controllers/EditorController.js
@@ -1,5 +1,5 @@
 angular.module('ccApp.controllers', ['LocalStorageModule']).
-  controller('EditorController', ['$scope', 'localStorageService', function($scope, localStorageService) {
+  controller('EditorController', ['$scope', 'localStorageService', '$timeout', function($scope, localStorageService, $timeout) {
     
     $scope.files = {
         html: '<h1>My First HTML Page</h1>',
@@ -7,11 +7,13 @@ angular.module('ccApp.controllers', ['LocalStorageModule']).
     };
     
     $scope.sessions = {};
-    
     $scope.currentTab = 'html';
     $scope.editor = null;
+    $scope.localStoragePromise = null;
     
     $scope.publish = function() {
+        $scope.save();
+        
         var frame = angular.element(document.querySelector('#previewWindow'))[0];
         var doc = frame.contentDocument || frame.contentWindow.document;
         angular.element(doc).find('body').html($scope.files.html);
@@ -25,10 +27,16 @@ angular.module('ccApp.controllers', ['LocalStorageModule']).
     $scope.changeTab = function(newTab) {
         $scope.currentTab = newTab;
         $scope.editor.setSession($scope.sessions[$scope.currentTab]);
-        //console.log(JSON.stringify($scope.sessions[$scope.currentTab]));
         $scope.files[$scope.currentTab] = $scope.editor.getValue();
-        localStorageService.add('data_cache',JSON.stringify($scope.files));
-        //$scope.editor.setValue($scope.files[newTab]);
+    }
+    
+    $scope.save = function() {
+        if ($scope.localStoragePromise) {
+            $timeout.cancel($scope.localStoragePromise);
+        }
+        $scope.localStoragePromise = $timeout(function() {
+            localStorageService.add('data_cache',JSON.stringify($scope.files));
+        });
     }
     
     $scope.aceLoaded = function(_editor){
@@ -36,28 +44,21 @@ angular.module('ccApp.controllers', ['LocalStorageModule']).
         $scope.editor = _editor;
         
         //Creating some sessions!
-        $scope.sessions['html'] = ace.createEditSession($scope.files['html']);
-        $scope.sessions['html'].setUndoManager(new ace.UndoManager()); 
-        $scope.sessions['css'] = ace.createEditSession($scope.files['css']);
-        $scope.sessions['css'].setUndoManager(new ace.UndoManager());
+        for (file in $scope.files) {
+            $scope.sessions[file] = ace.createEditSession($scope.files[file]);
+            $scope.sessions[file].setMode("ace/mode/" + file);
+            $scope.sessions[file].setUndoManager(new ace.UndoManager()); 
+        }
         
         $scope.changeTab($scope.currentTab);
     };
     
-    main = function() {
+    init = function() {
         var cache = localStorageService.get('data_cache');
         if (cache) {
             $scope.files = JSON.parse(cache);
-        }
-//        $scope.editor = ace.edit("editor");
-//        $scope.editor.setTheme("ace/theme/monokai");
-//        $scope.changeTab('html');
-//        
-        
+        }  
     };
     
-    
-    
-    main();
-    
+    init();
   }]);
